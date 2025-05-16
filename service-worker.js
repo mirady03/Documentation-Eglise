6d759d263116a9fbb98c8c1eaba0a9658572875e
mirady03 <miradykadima@gmail.com>
2025-05-17 00:52:02 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+// Liste des fichiers à mettre en cache
+const CACHE_NAME = 'church-doc-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Installation du service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Stratégie de cache: d'abord essayer le réseau, puis revenir au cache si hors ligne
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
